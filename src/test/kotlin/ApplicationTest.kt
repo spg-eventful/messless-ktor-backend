381857ea88cd332d1348d32eeb7655e8e724a46c
Ilja Busch <ilja.ragnar@gmail.com>
2025-11-27 10:43:02 +0100

    feat: add base testing infra and test application.kt for 200

---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,19 @@
+import at.eventful.messless.module
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("OK", response.bodyAsText())
+    }
+}
