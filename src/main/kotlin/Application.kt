1ed52195198d4bf9901627691ab60156a293de5e
Ilja Busch <ilja.ragnar@gmail.com>
2026-03-05 13:15:30 +0100

    fix: remove undefined service route reg.
    Signed-off-by: Ilja Busch <ilja.ragnar@gmail.com>

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,7 +4,6 @@ import at.eventful.messless.plugins.socket.WebSocketRouter
 import at.eventful.messless.plugins.socket.configureWebSocket
 import at.eventful.messless.services.echo.EchoService
 import at.eventful.messless.services.index.registerIndexRoute
-import at.eventful.messless.services.users.UsersService
 import io.ktor.server.application.*
 
 val router = WebSocketRouter()
@@ -21,5 +20,4 @@ fun Application.module() {
     // Register WS routes
     router.removeAllRoutes()
     router.register(EchoService())
-    router.register(UsersService())
 }
