e2e9843ab558125de2df6a2111964fd7706ea772
Christoph Leitgeb <christophleitgeb2007@gmail.com>
2026-03-12 10:35:05 +0100

    refactor: add return type for WebSocketResponse

--- src/main/kotlin/services/warehouse/WarehouseService.kt
+++ src/main/kotlin/services/warehouse/WarehouseService.kt
@@ -1,50 +1,64 @@
 package at.eventful.messless.services.warehouse
 
+import at.eventful.messless.errors.responses.BadRequest
+import at.eventful.messless.errors.responses.NotFound
 import at.eventful.messless.plugins.socket.ServiceMethod
 import at.eventful.messless.plugins.socket.WebSocketService
 import at.eventful.messless.plugins.socket.model.WebSocketResponse
 import at.eventful.messless.repositories.warehouse.WarehouseRepository
+import at.eventful.messless.schema.dto.WarehouseDto
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.plugins.di.dependencies
 import repositories.warehouse.command.CreateWarehouseCmd
 import repositories.warehouse.command.UpdateWarehouseCmd
 
-class WarehouseService : WebSocketService("warehouse") {
-    val warehouseRepository = WarehouseRepository()
+class WarehouseService(app: Application) : WebSocketService("warehouse") {
+    val warehouseRepository: WarehouseRepository by app.dependencies
 
-    override fun ServiceMethod.create(): WebSocketResponse {
+    override fun ServiceMethod.create(): WebSocketResponse<WarehouseDto> {
         val cmd = incoming.receiveBody<CreateWarehouseCmd>()
-        return WebSocketResponse(
-            HttpStatusCode.Created,
-            warehouseRepository.addWarehouse(cmd).toExposedWarehouse().toString()
-        )
+        try {
+            return WebSocketResponse.from(
+                HttpStatusCode.Created,
+                WarehouseDto.from(warehouseRepository.addWarehouse(cmd))
+            )
+        } catch (e: Exception) {
+            if (e.message?.contains("PUBLIC.USERS_EMAIL_UNIQUE") ?: false) {
+                throw BadRequest("A user with this email already exists.")
+            }
+            throw e
+        }
     }
 
-    override fun ServiceMethod.find(): WebSocketResponse {
-        return WebSocketResponse(
+    override fun ServiceMethod.find(): WebSocketResponse<List<WarehouseDto>> {
+        return WebSocketResponse.from(
             HttpStatusCode.OK,
-            warehouseRepository.allWarehouses().stream().map { it.toExposedWarehouse() }.toString()
+            warehouseRepository.allWarehouses().map(WarehouseDto::from)
         )
     }
 
-    override fun ServiceMethod.get(id: Int): WebSocketResponse {
-        return WebSocketResponse(
+    override fun ServiceMethod.get(id: Int): WebSocketResponse<WarehouseDto> {
+        val warehouse = warehouseRepository.warehouseById(id) ?: throw NotFound("Warehouse with id $id not found")
+        return WebSocketResponse.from(
             HttpStatusCode.OK,
-            warehouseRepository.warehouseById(id)?.toExposedWarehouse().toString()
+            WarehouseDto.from(warehouse)
         )
     }
 
-    override fun ServiceMethod.update(): WebSocketResponse {
-        val cmd = incoming.receiveBody<UpdateWarehouseCmd>()
-        return WebSocketResponse(
+    override fun ServiceMethod.update(id: Int): WebSocketResponse<WarehouseDto> {
+        val warehouse = warehouseRepository.updateWarehouse(id, incoming.receiveBody<UpdateWarehouseCmd>())
+            ?: throw NotFound("Warehouse with id $id not found")
+        return WebSocketResponse.from(
             HttpStatusCode.OK,
-            warehouseRepository.updateWarehouse(cmd).toExposedWarehouse().toString()
+            WarehouseDto.from(warehouse)
         )
     }
 
-    override fun ServiceMethod.delete(): WebSocketResponse {
+    override fun ServiceMethod.delete(id: Int): WebSocketResponse<Nothing> {
+        warehouseRepository.removeWarehouse(id) ?: throw NotFound("Warehouse with id $id not found")
         return WebSocketResponse(
-            HttpStatusCode.NoContent,
-            warehouseRepository.removeWarehouse(incoming.receiveBody<Int>()).toString()
+            HttpStatusCode.NoContent
         )
     }
 }
