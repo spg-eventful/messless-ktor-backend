0969bfd3be06b5bfb18cd8722503ee22d76758c9
Christoph Leitgeb <christophleitgeb2007@gmail.com>
2026-03-17 16:16:22 +0100

    refactor: add company to test users

--- src/test/kotlin/testutils/AuthorizationTests.kt
+++ src/test/kotlin/testutils/AuthorizationTests.kt
@@ -1,10 +1,12 @@
 package testutils
 
+import at.eventful.messless.schema.dao.CompanyDao
 import at.eventful.messless.schema.dao.UserDao
 import at.eventful.messless.schema.utils.UserRole
 
 open class AuthorizationTest {
-    val admin = UserDao.fake(1).copy(role = UserRole.Admin)
-    val owner = UserDao.fake(2).copy(role = UserRole.CompanyAdmin)
-    val stranger = UserDao.fake(3).copy(role = UserRole.Worker)
+    val company = CompanyDao.fake(1);
+    val admin = UserDao.fake(1).copy(role = UserRole.Admin, company = company)
+    val owner = UserDao.fake(2).copy(role = UserRole.CompanyAdmin, company = company)
+    val stranger = UserDao.fake(3).copy(role = UserRole.Worker, company = company)
 }
